d07e2756946bbc234efebfc39745fb414b90aafe
sky(skysurfr) <skysurfr@me.com>
2023-09-27 17:52:13 +0900

    :dizzy: 스타일 컴포넌트 타입 추가, 모션 바운스 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "styled-components";
+import { motion, spring } from "framer-motion";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+type StyledElement = ReturnType<typeof styled.div>;
+const S: Record<string, StyledElement> = {};
+S.Section = styled.section`
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+S.Button = styled.button`
+  background-color: tomato;
+  border: none;
+  width: 12.5rem;
+  height: 12.5rem;
+  box-shadow: inset -10px -10px 15px rgba(0, 0, 0, 0.2);
+  border-radius: 10px;
+`;
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <S.Section>
+      <S.Button
+        as={motion.button}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 360 }}
+        transition={{ type: "spring", bounce: 0.4 }}
+      >
+        Box1
+      </S.Button>
+    </S.Section>
+  );
+};
 
-export default App
+export default App;
