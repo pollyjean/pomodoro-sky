6fb9fb2825072be63dd0d20b3b07940eab238b9a
sky(skysurfr) <skysurfr@me.com>
2023-09-28 15:33:39 +0900

    :dizzy: Hover Tap 이벤트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components";
-import { motion, spring } from "framer-motion";
+import { motion } from "framer-motion";
 
 type StyledElement = ReturnType<typeof styled.div>;
 const S: Record<string, StyledElement> = {};
+
 S.Section = styled.section`
   display: flex;
   height: 100vh;
@@ -10,26 +11,35 @@ S.Section = styled.section`
   justify-content: center;
   align-items: center;
   width: 100%;
+  background-color: tomato;
 `;
 S.Button = styled.button`
-  background-color: tomato;
+  background-color: rgba(255, 255, 255, 0.9);
+  display: flex;
+  align-items: center;
+  justify-content: center;
   border: none;
   width: 12.5rem;
   height: 12.5rem;
-  box-shadow: inset -10px -10px 15px rgba(0, 0, 0, 0.2);
-  border-radius: 10px;
+  box-shadow: inset 2px 2px 4px rgba(0, 0, 0, 0.2);
+  border-radius: 3rem;
 `;
+
+const boxVariants = {
+  hover: {
+    scale: 1.2,
+    rotateZ: 90,
+  },
+  click: {
+    borderRadius: "50%",
+    scale: 0.9,
+  },
+};
+
 const App = () => {
   return (
     <S.Section>
-      <S.Button
-        as={motion.button}
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
-        transition={{ type: "spring", bounce: 0.4 }}
-      >
-        Box1
-      </S.Button>
+      <S.Button as={motion.button} variants={boxVariants} whileHover="hover" whileTap="click" />
     </S.Section>
   );
 };
