a585f9deddc16e0774a801a983c0ddd4dc0a1b5a
sky(skysurfr) <skysurfr@me.com>
2023-09-30 18:18:23 +0900

    :art: "S-dot AutoComplete: Styled-Components in TypeScript" 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,40 @@
 import { motion } from "framer-motion";
-import S from "./style";
-import styled from "styled-components";
 import { useRef } from "react";
+import styled from "styled-components";
 
-S.Section = styled.section`
-  display: flex;
-  height: 100vh;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  background-color: tomato;
-`;
-S.Wrap = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 30rem;
-  height: 30rem;
-  border: 1px solid rgba(0, 0, 0, 0.2);
-  overflow: hidden; // drag area district
-`;
-S.Button = styled.button`
-  background-color: rgba(255, 255, 255, 0.9);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: none;
-  width: 10rem;
-  height: 10rem;
-  box-shadow: inset 2px 2px 4px rgba(0, 0, 0, 0.2);
-  border-radius: 3rem;
-`;
+type StyledElement = ReturnType<typeof styled.div>;
+
+const S = {
+  Section: styled.section`
+    display: flex;
+    height: 100vh;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    background-color: tomato;
+  ` as StyledElement,
+  Wrap: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 30rem;
+    height: 30rem;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    overflow: hidden;
+  ` as StyledElement,
+  Button: styled.button`
+    background-color: rgba(255, 255, 255, 0.9);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    width: 10rem;
+    height: 10rem;
+    box-shadow: inset 2px 2px 4px rgba(0, 0, 0, 0.2);
+    border-radius: 3rem;
+  ` as StyledElement,
+};
 
 const boxVariants = {
   hover: {
@@ -42,12 +45,11 @@ const boxVariants = {
     scale: 0.9,
   },
   drag: {
-    backgroundColor: "rgba(0,0,255, 0.2)", // color is number type
+    backgroundColor: "rgba(0,0,255, 0.2)",
     transition: {
       duration: 2,
     },
   },
-  area: {},
 };
 
 const App = () => {
